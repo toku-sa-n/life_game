cf3e288e696d6dbc3fb81dff667b3ee02d466e4d
toku-sa-n <tokuchan3515@gmail.com>
2019-07-06 09:22:31 +0900

    init.

---
+++ life_game_lib.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+void** two_dimension_calloc(size_t __height, size_t __width, size_t __size)
+{
+    void** two_dimension_array = calloc(__height, sizeof(__size));
+    if (two_dimension_array == NULL) {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < __height; i++) {
+        two_dimension_array[i] = calloc(__width, sizeof(__size));
+
+        if (two_dimension_array[i] == NULL) {
+            // Free memories which have already allocated.
+            // Not include two_dimension_array[i]
+            // because two_dimension_array[j] is NULL.
+            for (size_t j = 0; j < i; j++) {
+                free(two_dimension_array[j]);
+            }
+            free(two_dimension_array);
+            return NULL;
+        }
+    }
+
+    return two_dimension_array;
+}
+
+// Free the memories of array which were allocated by two_dimension_calloc function.
+void free_two_dimension_array(size_t __height, void** __array)
+{
+    for (size_t i = 0; i < __height; i++) {
+        free(__array[i]);
+    }
+    free(__array);
+}
---
+++ life_game_lib.h
@@ -0,0 +1,8 @@
+#ifndef __LIFE_GAME_LIB
+#define __LIFE_GAME_LIB
+
+#include <stddef.h>
+
+void** two_dimension_calloc(size_t __height, size_t __width, size_t __size);
+void free_two_dimension_array(size_t __height, void** __array);
+#endif
