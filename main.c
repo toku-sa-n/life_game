18280ee0a63658f61bd70d0b0a040665901ef9ae
toku-sa-n <tokuchan3515@gmail.com>
2019-07-06 12:09:40 +0900

    Complete life game with ncurses

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include "life_game_lib.h"
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int main(void)
 {
-    int height = 20;
-    int width = 20;
+    initscr();
+    cbreak();
+    noecho();
+    refresh();
+
+    int height = LINES;
+    int width = COLS;
 
     // 1: Alive 0: Dead
     int** life_game_table = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
@@ -19,13 +26,17 @@ int main(void)
     init_life_game(height, width, life_game_table);
 
     for (int i = 0; i < 100; i++) {
-        print_table(height, width, life_game_table);
+        print_table_with_ncurses(height, width, life_game_table);
         putchar('\n');
         proceed_generation(height, width, life_game_table, life_game_next_generation_table);
         add_two_array(height, width, life_game_table, life_game_next_generation_table);
+        refresh();
+        usleep(100000);
     }
 
     free_two_dimension_array((size_t)height, (void**)life_game_table);
     free_two_dimension_array((size_t)height, (void**)life_game_next_generation_table);
+
+    endwin();
     return 0;
 }
