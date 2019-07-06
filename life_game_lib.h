5baefc788fd589c69cd63c5d5182a1591c46595a
toku-sa-n <tokuchan3515@gmail.com>
2019-07-06 12:09:19 +0900

    Add declaration of print_table_with_ncurses

--- life_game_lib.h
+++ life_game_lib.h
@@ -7,6 +7,7 @@ void** two_dimension_calloc(size_t __height, size_t __width, size_t __size);
 void free_two_dimension_array(size_t __height, void** __array);
 void init_life_game(int __height, int __width, int** __array);
 void print_table(int __height, int __width, int** array);
+void print_table_with_ncurses(int __height, int __width, int** __array);
 void proceed_generation(int __height, int __width, int** __current_generation_table, int** __next_generation_table);
 void add_two_array(int __height, int __width, int** __array_1, int** __array_2);
 #endif
