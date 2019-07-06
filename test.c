74b43f4774e3b2f0376e9751a22e95800cac82b2
toku-sa-n <tokuchan3515@gmail.com>
2019-07-06 09:22:49 +0900

    Add two_dimension_calloc_test test

--- test.c
+++ test.c
@@ -1,7 +1,19 @@
+#include "life_game_lib.h"
 #include <stdio.h>
+
+void two_dimension_calloc_test(void)
+{
+    int** two_dimension_array = (int**)two_dimension_calloc(50000, 50000, sizeof(int));
+    for (int i = 0; i < 50000; i++) {
+        for (int j = 0; j < 50000; j++) {
+            two_dimension_array[i][j] = i + j;
+        }
+    }
+    free_two_dimension_array((size_t)50000, (void**)two_dimension_array);
+}
 
 int main(void)
 {
-
+    two_dimension_calloc_test();
     return 0;
 }
