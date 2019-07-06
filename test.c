f8df3701d6118c3c301bda9536e837d0e487470f
toku-sa-n <tokuchan3515@gmail.com>
2019-07-06 14:28:48 +0900

    Fix test file.

--- test.c
+++ test.c
@@ -1,19 +1,19 @@
 #include "life_game_lib.h"
 #include <stdio.h>
 
-void two_dimension_calloc_test(void)
+void two_dimension_calloc_int_test(void)
 {
     int height = 50000;
     int width = 50000;
-    int** two_dimension_array = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
-    free_two_dimension_array((size_t)height, (void**)two_dimension_array);
+    int** two_dimension_array = two_dimension_calloc_int((size_t)height, (size_t)width);
+    free_two_dimension_array((size_t)height, two_dimension_array);
 }
 
 void init_life_game_test(void)
 {
     int height = 10;
     int width = 10;
-    int** life_game_table = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
+    int** life_game_table = two_dimension_calloc_int((size_t)height, (size_t)width);
     init_life_game(height, width, life_game_table);
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -22,14 +22,14 @@ void init_life_game_test(void)
         putchar('\n');
     }
     putchar('\n');
-    free_two_dimension_array((size_t)height, (void**)life_game_table);
+    free_two_dimension_array((size_t)height, life_game_table);
 }
 
 void print_table_test(void)
 {
     int height = 10;
     int width = 10;
-    int** array = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
+    int** array = two_dimension_calloc_int((size_t)height, (size_t)width);
 
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
@@ -39,15 +39,15 @@ void print_table_test(void)
     print_table(height, width, array);
     putchar('\n');
 
-    free_two_dimension_array((size_t)height, (void**)array);
+    free_two_dimension_array((size_t)height, array);
 }
 
 void proceed_generation_test(void)
 {
     int height = 5;
     int width = 5;
-    int** life_game_table = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
-    int** next_generation_table = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
+    int** life_game_table = two_dimension_calloc_int((size_t)height, (size_t)width);
+    int** next_generation_table = two_dimension_calloc_int((size_t)height, (size_t)width);
 
     init_life_game(height, width, life_game_table);
     proceed_generation(height, width, life_game_table, next_generation_table);
@@ -66,8 +66,8 @@ void proceed_generation_test(void)
         putchar('\n');
     }
 
-    free_two_dimension_array((size_t)height, (void**)life_game_table);
-    free_two_dimension_array((size_t)height, (void**)next_generation_table);
+    free_two_dimension_array((size_t)height, life_game_table);
+    free_two_dimension_array((size_t)height, next_generation_table);
 }
 
 void add_two_array_test(void)
@@ -75,8 +75,8 @@ void add_two_array_test(void)
     int height = 5;
     int width = 5;
 
-    int** array_1 = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
-    int** array_2 = (int**)two_dimension_calloc((size_t)height, (size_t)width, sizeof(int));
+    int** array_1 = two_dimension_calloc_int((size_t)height, (size_t)width);
+    int** array_2 = two_dimension_calloc_int((size_t)height, (size_t)width);
 
     init_life_game(height, width, array_1);
     init_life_game(height, width, array_2);
@@ -91,13 +91,13 @@ void add_two_array_test(void)
     puts("array_1 + array_2");
     print_table(height, width, array_1);
 
-    free_two_dimension_array((size_t)height, (void**)array_1);
-    free_two_dimension_array((size_t)height, (void**)array_2);
+    free_two_dimension_array((size_t)height, array_1);
+    free_two_dimension_array((size_t)height, array_2);
 }
 
 int main(void)
 {
-    two_dimension_calloc_test();
+    two_dimension_calloc_int_test();
     init_life_game_test();
     print_table_test();
     proceed_generation_test();
